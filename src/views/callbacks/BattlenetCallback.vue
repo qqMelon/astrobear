b22baa0199ee6abe354c76b9978a8026a4ea949d
qqMelon <urie.zahard@gmail.com>
2025-07-07 17:04:32 +0200

    revert for link button

--- src/views/callbacks/BattlenetCallback.vue
+++ src/views/callbacks/BattlenetCallback.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { onMounted } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
+import { useRoute } from 'vue-router'
 import { useToastStore } from '@/stores/toast'
 
 const route = useRoute()
