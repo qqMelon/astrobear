91ffaeba183a5884352cb8fba58f7383e2b76e64
qqMelon <urie.zahard@gmail.com>
2025-07-18 19:43:54 +0200

    Pull from Directus before Blizzard

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -10,6 +10,7 @@ const authStore = useAuthStore()
 
 const token = getValidBattlenetToken()
 
+const authToken = authStore.token || localStorage.getItem('astrobear-user-token')
 const bnetLinked = ref(false)
 const characters = ref([])
 const isLoading = ref(false)
@@ -17,9 +18,9 @@ const error = ref(null)
 const selectedChar = ref(null)
 const alreadyCalled = ref(false)
 
-const syncCharacters = function () {
+const syncCharacters = async function () {
   if (alreadyCalled.value === true) return
-  loadChars()
+  console.log('To update')
 }
 
 const loadChars = async function () {
@@ -33,35 +34,55 @@ const loadChars = async function () {
   }
 
   try {
-    const res = await fetch(
-      'https://eu.api.blizzard.com/profile/user/wow?namespace=profile-eu&local=fr_FR',
-      {
+    let res = await fetch(import.meta.env.VITE_BACKEND_BASE_URL + `/items/wow_characters?filter[user][_eq]=${
+authStore.user.id
+}`, {
         headers: {
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${authToken}`,
         },
       }
     )
 
-    if (!res.ok) {
-      throw new Error('Impossible de charger les personnages.')
+    if (!res.ok) throw new Error('Erreur lors du chargement des personnages depuis Directus.')
+
+    console.log('chars data exist from Directus')
+
+    let data = await res.json()
+
+    if (data.data && data.data.length > 0) {
+      characters.value = data.data
+    } else {
+      console.log('chars data doesnt exist, pull from Blizzard API')
+      const res = await fetch(
+        'https://eu.api.blizzard.com/profile/user/wow?namespace=profile-eu&local=fr_FR',
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+
+      if (!res.ok) {
+        throw new Error('Impossible de charger les personnages.')
+      }
+
+      const data = await res.json()
+      const formattedData = data.wow_accounts[0].characters.map((char) => ({
+        char_id: char.id,
+        name: char.name,
+        level: char.level,
+        realm: char.realm.name.fr_FR,
+        class: char.playable_class.name.fr_FR,
+        race: char.playable_race.name.fr_FR,
+        faction: char.faction.name.fr_FR,
+        is_main: false,
+        href: char.character.href,
+        user: authStore.user.id,
+      }))
+
+      await uploadToDirectus(formattedData)
+      characters.value = formattedData
     }
-
-    const data = await res.json()
-    const formattedData = data.wow_accounts[0].characters.map((char) => ({
-      char_id: char.id,
-      name: char.name,
-      level: char.level,
-      realm: char.realm.name.fr_FR,
-      class: char.playable_class.name.fr_FR,
-      race: char.playable_race.name.fr_FR,
-      faction: char.faction.name.fr_FR,
-      is_main: false,
-      href: char.character.href,
-      user: authStore.user.id,
-    }))
-
-    await syncToDirectus(formattedData)
-    characters.value = formattedData
   } catch (err) {
     error.value = err.message
   } finally {
@@ -73,9 +94,7 @@ const loadChars = async function () {
   }
 }
 
-const syncToDirectus = async function (charactersList) {
-  const authToken = authStore.token || localStorage.getItem('astrobear-user-token')
-
+const uploadToDirectus = async function (charactersList) {
   try {
     const res = await fetch(import.meta.env.VITE_BACKEND_BASE_URL + '/items/wow_characters?batch=all', {
       method: 'POST',
@@ -88,7 +107,6 @@ const syncToDirectus = async function (charactersList) {
 
     const data = await res.json()
     if (!res.ok) throw new Error(data.error?.message || 'Erreur Directus')
-    console.log('✅ Personnages synchronisés dans Directus', data)
   } catch (err) {
     console.error('❌ Erreur de synchro Directus: ', err)
   }
@@ -98,6 +116,7 @@ const sortedCharacters = computed(() => [...characters.value].sort((a, b) => b.l
 
 onMounted(() => {
   if (token) bnetLinked.value = true
+  loadChars()
 })
 </script>
 
@@ -112,9 +131,9 @@ onMounted(() => {
     <div v-else>
       <button class="uk-button uk-button-primary" @click="syncCharacters">
         {{
-          !alreadyCalled
-            ? 'Synchroniser mes personnages'
-            : 'Patientez avant de relancer une synchronisation'
+        !alreadyCalled
+        ? 'Synchroniser mes personnages'
+        : 'Patientez avant de relancer une synchronisation'
         }}
       </button>
       <div class="uk-grid-large" uk-grid>
@@ -127,10 +146,10 @@ onMounted(() => {
               v-for="char in sortedCharacters"
               :key="char.char_id"
               :character="char"
-              :class="{
+              :class=" {
                 'uk-card-primary': selectedChar?.id === char.char_id,
               }"
-              style="cursor: pointer"
+              style="   cursor: pointer"
               @click="selectedChar = char"
             />
           </div>
